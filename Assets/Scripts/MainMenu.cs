3b22c06dc9665f54d997b408e0ddd19635ca8dc1
atlass-dev <73686943+atlass-dev@users.noreply.github.com>
2020-12-29 16:52:18 +0500

    first

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] int money;
+    public int total_money;
+    public Text moneyText;
+
+    private void Start()
+    {
+        money = PlayerPrefs.GetInt("money");
+        total_money = PlayerPrefs.GetInt("total_money");
+        bool isFirst = PlayerPrefs.GetInt("isFirst") == 1 ? true : false;
+        if (isFirst)
+        {
+            StartCoroutine(IdleFarm());
+        }
+        OfflineTime();
+    }
+
+    public void ButtonClick()
+    {
+        money++;
+        total_money++;
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("total_money", total_money);
+    }
+
+    IEnumerator IdleFarm()
+    {
+        yield return new WaitForSeconds(1);
+        money++;
+        Debug.Log(money);
+        PlayerPrefs.SetInt("money", money);
+        StartCoroutine(IdleFarm());
+    }
+
+    private void OfflineTime()
+    {
+        TimeSpan ts;
+        if (PlayerPrefs.HasKey("LastSession"))
+        {
+            ts = DateTime.Now - DateTime.Parse(PlayerPrefs.GetString("LastSession"));
+            Debug.Log(string.Format("Вас не было {0} дней, {1} часов, {2} минут, {3} секунд", ts.Days, ts.Hours, ts.Minutes, ts.Seconds));
+            money += (int)ts.TotalSeconds;
+        }
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            PlayerPrefs.SetString("LastSession", DateTime.Now.ToString());
+        }
+    }
+#else
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString("LastSession", DateTime.Now.ToString());
+    }
+#endif
+
+    public void ToAchievements()
+    {
+        PlayerPrefs.SetString("LastSession", DateTime.Now.ToString());
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+}
